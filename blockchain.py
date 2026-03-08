f32cb19438dff12d4b7fdae658ed3308a0218df4
S J Rishika <164775589+rishikasj7606@users.noreply.github.com>
2026-03-09 01:36:57 +0530

    Refactor Block class for better hash computation
    Refactor hash computation and add to_dict method.

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,7 @@ import hashlib
 import json
 from time import time
 
+
 class Block:
     def __init__(self, index, timestamp, data, previous_hash):
         self.index = index
@@ -11,8 +12,27 @@ class Block:
         self.hash = self.compute_hash()
 
     def compute_hash(self):
-        block_string = json.dumps(self.__dict__, sort_keys=True)
+        block_string = json.dumps(
+            {
+                "index": self.index,
+                "timestamp": self.timestamp,
+                "data": self.data,
+                "previous_hash": self.previous_hash,
+            },
+            sort_keys=True,
+            default=str,
+        )
         return hashlib.sha256(block_string.encode()).hexdigest()
+
+    def to_dict(self):
+        return {
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "data": self.data,
+            "previous_hash": self.previous_hash,
+            "hash": self.hash,
+        }
+
 
 class Blockchain:
     def __init__(self):
@@ -25,5 +45,23 @@ class Blockchain:
 
     def add_block(self, data):
         previous_block = self.chain[-1]
-        new_block = Block(len(self.chain), time(), data, previous_block.hash)
+        new_block = Block(
+            len(self.chain),
+            time(),
+            data,
+            previous_block.hash
+        )
         self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i - 1]
+
+            if current.hash != current.compute_hash():
+                return False
+
+            if current.previous_hash != previous.hash:
+                return False
+
+        return True
